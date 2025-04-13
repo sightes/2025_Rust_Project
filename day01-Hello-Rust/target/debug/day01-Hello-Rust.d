c90b42991d723e9704e89c9052d443f084d51196
Sightes <sightes@gmail.com>
2025-04-12 22:49:30 -0400

    sdf
    sdf

--- day01-Hello-Rust/target/debug/day01-Hello-Rust.d
+++ day01-Hello-Rust/target/debug/day01-Hello-Rust.d
@@ -1,1 +1,1 @@
-/Users/sightes/Documents/GITHUB/2025_Rust_Project/day01-Hello-Rust/target/debug/day01-Hello-Rust: /Users/sightes/Documents/GITHUB/2025_Rust_Project/day01-Hello-Rust/src/main.rs
+/Users/sebastianulloa/Documents/GITHUB/2025_Rust_Project/day01-Hello-Rust/target/debug/day01-Hello-Rust: /Users/sebastianulloa/Documents/GITHUB/2025_Rust_Project/day01-Hello-Rust/src/main.rs
---
+++ day01-Hello-Rust/target/debug/deps/day01_Hello_Rust-3e2a88c57db03648.d
@@ -0,0 +1,5 @@
+/Users/sebastianulloa/Documents/GITHUB/2025_Rust_Project/day01-Hello-Rust/target/debug/deps/day01_Hello_Rust-3e2a88c57db03648: src/main.rs
+
+/Users/sebastianulloa/Documents/GITHUB/2025_Rust_Project/day01-Hello-Rust/target/debug/deps/day01_Hello_Rust-3e2a88c57db03648.d: src/main.rs
+
+src/main.rs:
